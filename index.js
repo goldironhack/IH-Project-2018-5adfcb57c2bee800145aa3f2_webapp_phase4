8fa957d1bc38ca127a2d0a7e42aa377282491618
walterzt <cheung_98@163.com>
2018-05-24 22:33:30 -0400

    Automatic commit. Done before go to queltrics

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
-    });
-})();
+body{
+    margin: 0px;
+    }
+h1{
+    color: white;
+    text-align: center;
+    line-height: 50px;
+    font-family: "Microsoft Yahei";
+}
+.uptitle{
+    background-color: #FF570C;
+    height: 50px;
+}
+.menu{
+    background-color: #606470;
+    height: 700px;
+    width: 400px;
+    font-family: "Microsoft Yahei";
+    float: left;
+}
+#map{
+    background-color: #F7F7F7;
+    height: 700px;
+    width: 1000px;
+    font-family: "Microsoft Yahei";
+    float: right;
+}
+.container{
+    width: 1400px;
+    margin: 0 auto;
+}
